5abf78c043bf547d6995437f88fa3b06082ec684
lamidu rathnayake <lamidurathnayakeofficial@gmail.com>
2026-02-25 23:13:33 +0530

    i added initial console application program and update readme file

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Console-Application-DSA-Group-Assignment-SLTC
 
-We created a console based java application with three modules.  
+We created a console based java application with three modules as follows,
+
+    module 1 : Smart City Route Planner
+    module 2 : Data Sorter – Sorting Algorithm Comparison Tool
+    module 3 : Algorithm Performance Analyzer (Time Complexity)
+
--- src/ConsoleApp/ConsoleApp.java
+++ src/ConsoleApp/ConsoleApp.java
@@ -6,33 +6,45 @@ import Module3.Module3Manager;
 
 public class ConsoleApp {
     public ConsoleApp() {
-        Scanner sc = new Scanner(System.in);
-
-        while (true) { 
-            System.out.println("\n--- Console Application ---"); 
-            System.out.println("1. Module 1"); 
-            System.out.println("2. Module 2"); 
-            System.out.println("3. Module 3"); 
-            System.out.println("4. Exit"); 
-            System.out.print("Choose a module: "); 
-            int choice = sc.nextInt(); sc.nextLine(); // consume newline 
-            
-            switch (choice) { 
-                case 1: 
-                    new Module1Manager();
-                    break; 
-                case 2: 
-                    new Module2Manager();
-                    break; 
-                case 3: 
-                    new Module3Manager();
-                    break; 
-                case 4: 
-                    System.out.println("Exiting application. Goodbye!"); 
-                    return; 
-                default: 
-                    System.out.println("Invalid choice. Please try again."); 
-            } 
+        while (true) {
+            try {
+                while (true) { 
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("\n------------------------------------------------------------------");
+                    System.out.println("---------------------- CONSOLE APPLICATION -----------------------"); 
+                    System.out.println("------------------------------------------------------------------");
+                    System.out.println("\t1. Smart City Route Planner"); 
+                    System.out.println("\t2. Data Sorter – Sorting Algorithm Comparison Tool"); 
+                    System.out.println("\t3. Algorithm Performance Analyzer (Time Complexity)"); 
+                    System.out.println("\t4. Exit"); 
+                    System.out.println("------------------------------------------------------------------");
+                    System.out.print("Choose a module: "); 
+                    int choice = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("------------------------------------------------------------------");
+                    System.out.println("");
+                    
+                    switch (choice) { 
+                        case 1: 
+                            new Module1Manager();
+                            break; 
+                        case 2: 
+                            new Module2Manager();
+                            break; 
+                        case 3: 
+                            new Module3Manager();
+                            break; 
+                        case 4: 
+                            System.out.println("Exiting application. Goodbye!"); 
+                            return; 
+                        default: 
+                            System.out.println("Invalid choice. Please try again."); 
+                    } 
+                }
+                
+            } catch (Exception e) {
+                System.out.println("Invalid user input");
+            }   
         }
     }
 }
