4334f855c5afcd76ef5fa788389e593d74b6efef
lamidu rathnayake <lamidurathnayakeofficial@gmail.com>
2026-02-28 18:54:11 +0530

    i added basic location/ road adding deleting options

--- src/ConsoleApp/ConsoleApp.java
+++ src/ConsoleApp/ConsoleApp.java
@@ -5,46 +5,57 @@ import Module2.Module2Manager;
 import Module3.Module3Manager;
 
 public class ConsoleApp {
+    public static final String RESET = "\u001B[0m"; 
+    public static final String BLACK = "\u001B[30m"; 
+    public static final String RED = "\u001B[31m"; 
+    public static final String GREEN = "\u001B[32m"; 
+    public static final String YELLOW = "\u001B[33m"; 
+    public static final String BLUE = "\u001B[34m"; 
+    public static final String PURPLE = "\u001B[35m"; 
+    public static final String CYAN = "\u001B[36m"; 
+    public static final String WHITE = "\u001B[37m";
+
     public ConsoleApp() {
+        Scanner sc = new Scanner(System.in);
+        int choice;        
+
         while (true) {
             try {
-                while (true) { 
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println("\n------------------------------------------------------------------");
-                    System.out.println("---------------------- CONSOLE APPLICATION -----------------------"); 
-                    System.out.println("------------------------------------------------------------------");
-                    System.out.println("");
-                    System.out.println("\t1. Smart City Route Planner"); 
-                    System.out.println("\t2. Data Sorter – Sorting Algorithm Comparison Tool"); 
-                    System.out.println("\t3. Algorithm Performance Analyzer (Time Complexity)"); 
-                    System.out.println("\t4. Exit"); 
-                    System.out.println("");
-                    System.out.print("\tChoose a module: "); 
-                    int choice = sc.nextInt();
-                    sc.nextLine();
-                    
-                    switch (choice) { 
-                        case 1: 
-                            new Module1Console();
-                            break; 
-                        case 2: 
-                            new Module2Manager();
-                            break; 
-                        case 3: 
-                            new Module3Manager();
-                            break; 
-                        case 4: 
-                            System.out.println("\tExiting application. Goodbye!"); 
-                            return; 
-                        default: 
-                            System.out.println("\tInvalid choice. Please try again."); 
-                    } 
-                }
+                System.out.println(YELLOW + "\n------------------------------------------------------------------");
+                System.out.println("---------------------- CONSOLE APPLICATION -----------------------"); 
+                System.out.println("------------------------------------------------------------------" + RESET);
+                System.out.println("");
+                System.out.println("\t1. Smart City Route Planner"); 
+                System.out.println("\t2. Data Sorter – Sorting Algorithm Comparison Tool"); 
+                System.out.println("\t3. Algorithm Performance Analyzer (Time Complexity)"); 
+                System.out.println("\t4. Exit"); 
+                System.out.println("");
+                System.out.print(YELLOW + "\tChoose A Module: " + RESET); 
+
+                choice = sc.nextInt();
+                sc.nextLine();
+
+                switch (choice) { 
+                    case 1: 
+                        new Module1Console(sc);
+                        break; 
+                    case 2: 
+                        new Module2Manager();
+                        break; 
+                    case 3: 
+                        new Module3Manager();
+                        break; 
+                    case 4: 
+                        System.out.println("\tExiting From The Application. Goodbye!"); 
+                        sc.close();
+                        return; 
+                    default: 
+                        System.out.println(RED + "\tInvalid Choice. Please Try Again." + RESET); 
+                } 
                 
             } catch (Exception e) {
-                System.out.println("\tInvalid user input");
-                System.out.println(e.getLocalizedMessage());
-                System.out.println(e.getMessage());
+                System.out.println(RED + "\tInvalid User Input" + RESET);
+                sc.nextLine(); // removing hte invalid input characters
             }   
         }
     }
--- src/Module1/Module1Console.java
+++ src/Module1/Module1Console.java
@@ -1,93 +1,237 @@
 package Module1;
-
 import java.util.Scanner;
 
-public class Module1Console {
+public class Module1Console{
+    public static final String RESET = "\u001B[0m"; 
+    public static final String BLACK = "\u001B[30m"; 
+    public static final String RED = "\u001B[31m"; 
+    public static final String GREEN = "\u001B[32m"; 
+    public static final String YELLOW = "\u001B[33m"; 
+    public static final String BLUE = "\u001B[34m"; 
+    public static final String PURPLE = "\u001B[35m"; 
+    public static final String CYAN = "\u001B[36m"; 
+    public static final String WHITE = "\u001B[37m";
+
     boolean freshLoad = true;
-    public Module1Console() {
-        System.out.println("\n-------------------- Smart City Route Planner --------------------");
+
+    public Module1Console(Scanner sc) {
+        System.out.println(YELLOW + "\n-------------------- Smart City Route Planner --------------------" + RESET);
+        
         while (true) {
-            Scanner sc = new Scanner(System.in);
             
             if (freshLoad) {
                 System.out.println("\n\t1. Add Location ");
                 freshLoad = false;
             }
             else { 
+                System.out.println(YELLOW + "\n\t-----------------------------------------------");
+                System.out.println(YELLOW + "\n\tMore Operations");
+                System.out.println(RESET);
                 System.out.println("\t1. Add Location ");
             }
             System.out.println("\t2. Remove Location ");
             System.out.println("\t3. Add Road ");
             System.out.println("\t4. Remove Road ");
             System.out.println("\t5. Find Best Route ");
-            System.out.println("\t6. Display Location Summery ");
-            System.out.println("\t7. Exit \n");
-            System.out.print("\tChoose A Operation: "); 
+            System.out.println("\t6. Exit \n");
+            System.out.print(YELLOW + "\tChoose A Operation: " + RESET); 
             int choice = sc.nextInt();
             sc.nextLine();
-            
+            System.out.println(YELLOW + "\n\t-----------------------------------------------");
             
             switch (choice) { 
                 case 1: 
-                    System.out.println("\n\tAdding Location (Press 'dn' to quit)");
-                    System.out.println("\t------------------------------------------------");
-                    
-                    int addedLocation = addLocation();
-                    System.out.println(String.format("\n\t%s Locations Were Added", addedLocation));
+                    addLocation(sc);
+                    break;
 
-                    System.out.println("\n\tMore Operations");
-                    System.out.println("\t------------------------------------------------");
-
-                    break; 
                 case 2: 
-                    // remove location
+                    removeLocation(sc);
                     break; 
+
                 case 3: 
-                    // add road
+                    addRoad(sc);
                     break; 
+
                 case 4: 
-                    // remove road
+                    removeRoad(sc);
                     break; 
+
                 case 5: 
-                    // find road
+                    traversal();
                     break; 
+
                 case 6: 
-                    // summery display
-                    break; 
-                case 7: 
                     System.out.println("\tExiting Module. Goodbye!"); 
                     return; 
+
                 default: 
-                    System.out.println("\tInvalid Choice. Please Try Again."); 
+                    System.out.println(RED + "\tInvalid Choice. Please Try Again." + RESET); 
             
             }
         }
     }
     
 
-    // adding location
-    public int addLocation() {
+    public void addLocation(Scanner scan) {
+        System.out.println(YELLOW + "\n\tAdding Locations (Press 'dn' to quit)");
+        System.out.println(RESET);
+                    
         int addedLocationCount = 0;
         while (true) {
             try {
-                Scanner scan = new Scanner(System.in);
+                // inputting the location: 
                 System.out.print("\tLocation Name: ");
                 String locationName = scan.next();
                 
+                // consuming the \n
                 scan.nextLine();
 
-                if (locationName.toUpperCase().equals("DN")) 
-                    return addedLocationCount;
+                // exit from location adding process if the user enter dn or DN
+                if (locationName.toUpperCase().equals("DN")) break;
 
-                if (LocationManager.addLocation(locationName) == 0)
-                    System.out.println("\t\tLocation Does Exist. Try Another!");
-                
-                addedLocationCount++;
+                // if location manager returned 0, it means it did do the task (location is already in the tree and adj.list)
+                // simply ignore the rest of the process and re start the location adding process wit new location
+                if (LocationManager.addLocation(locationName) == 0) {
+                    System.out.println(RED + "\t\tLocation Does Exist. Try Another!" + RESET);
+                    continue;
+                }
+                else {
+                    // if the locationManager returned 1, it means it added the location. now we have to increment the addedLocationCount by 1 
+                    // then the program will continue with a new location 
+                    addedLocationCount++;
+                    continue;
+                }
                     
             } catch (Exception e) {
-                System.out.println(String.format("\t\tSomething Happened! (%s Locations Were Added)", addedLocationCount));
+                // if the process encounts a error during the runtime, this catch block will ignore the problem and re-try with a new location adding process
+                System.out.println(String.format(RED +"\t\tSomething Happened! (%s Locations Were Added)" + RESET, addedLocationCount));
                 continue;
             }
         }
+
+        // finally we have this line to show how much of locations we added during the current location adding process
+        System.out.println(String.format(GREEN + "\n\t%s Locations Were Added" + RESET, addedLocationCount));
     }
+
+    public void removeLocation(Scanner scan) {
+        System.out.println(YELLOW + "\n\tRemoving Locations (Press 'dn' to quit)");
+        System.out.println(RESET);
+                    
+        int removedLocationCount = 0;
+        while (true) {
+            try {
+                System.out.print("\tLocation Name: ");
+                String locationName = scan.next();
+                scan.nextLine();
+
+                if (locationName.toUpperCase().equals("DN")) 
+                    break;
+
+                if (LocationManager.removeLocation(locationName) == 0) {
+                    System.out.println(RED + "\t\tLocation Does Not Exist. Try Another!" + RESET);
+                    continue;
+                }
+                else {
+                    removedLocationCount++;
+                    continue;
+                }
+
+                    
+            } catch (Exception e) {
+                System.out.println(String.format(RED + "\t\tSomething Happened! (%s Locations Were Deleted)" + RESET, removedLocationCount));
+                scan.nextLine();
+                continue;
+            }
+        }
+
+        System.out.println(String.format(GREEN + "\n\t%s Locations Were deleted" + RESET, removedLocationCount));
+    }
+
+
+    public void addRoad(Scanner scan) {
+        System.out.println(YELLOW + "\n\tAdding Roads (Press 'dn' to quit)");
+        System.out.println(RESET);
+
+        // while --> try catch
+        int addedRoadCount = 0;
+        while (true) {
+            try {
+                // asking the source location
+                System.out.print("\tSource Location: ");
+                String sourceLocation = scan.next();
+                scan.nextLine();
+                if (sourceLocation.toUpperCase().equals("DN")) break;
+                
+                // asking the destination
+                System.out.print("\tDestination location: ");
+                String destinationLocation = scan.next();
+                scan.nextLine();
+                if (destinationLocation.toUpperCase().equals("DN")) break;
+
+                // asking the distance 
+                System.out.print("\tdistance: ");
+                int distance = scan.nextInt();
+                scan.nextLine();
+                if (distance == -1) break;
+                
+                LocationManager.addRoad(sourceLocation, destinationLocation, distance);
+                addedRoadCount ++;
+                System.out.println(); // putting space after each set of road insertions
+                
+            } catch (Exception e) {
+                System.out.println(String.format(RED + "\t\tSomething Happened! (%s Roads Were Added)" + RESET, addedRoadCount));
+                scan.nextLine();
+                continue;
+            }
+        }
+
+        // printing the list and number of added roads at the end
+        LocationManager.printAdjacencyList();
+        System.out.println(String.format(GREEN + "\n\t%s Roads Were Added" + RESET, addedRoadCount));
+    }
+
+    public void removeRoad(Scanner scan) {
+        System.out.println(YELLOW + "\n\tRemoving Locations (Press 'dn' to quit)");
+        System.out.println(RESET);
+
+        // while --> try catch
+        int removedRoadsCount = 0;
+        while (true) {
+            try {
+                System.out.print("\tSource Location Name: ");
+                String sourceLocationName = scan.next();
+                scan.nextLine();
+                if (sourceLocationName.toUpperCase().equals("DN")) break;
+
+                System.out.print("\tDestination Location Name: ");
+                String destinationLocationName = scan.next();
+                scan.nextLine();
+                if (destinationLocationName.toUpperCase().equals("DN")) break;
+                
+                if (LocationManager.removeRoad(sourceLocationName,destinationLocationName) == 0) {
+                    System.out.println(RED + "\t\tLocation Does Not Exist. Try Another!" + RESET);
+                    continue;
+                }
+                else {
+                    removedRoadsCount++;
+                    continue;
+                }
+
+            } catch (Exception e) {
+                System.out.println(String.format(RED + "\t\tSomething Happened! (%s Roads Were Deleted)" + RESET, removedRoadsCount));
+                scan.nextLine();
+                continue;
+            }
+        }
+        
+        System.out.println(String.format(GREEN + "\n\t%s Roads Were deleted" + RESET, removedRoadsCount));
+    }
+
+
+    public void traversal() {
+        System.out.println("\n\t Traveling through the map");
+        LocationManager.traversal();
+        System.out.println();
+    }
+
 }
